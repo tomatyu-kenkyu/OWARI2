3462408bb32753ee5cc21fdafff1bf75d553bac1
tomatyu-kenkyu <tomatyu2008@gmail.com>
2026-05-11 07:24:32 +0000

    ｈ

--- app.py
+++ app.py
@@ -1,3 +1,44 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+from paddleocr import PaddleOCR
 
-st.title("起動成功")
+st.title("軽量PaddleOCR")
+
+@st.cache_resource
+def load_ocr():
+    return PaddleOCR(
+        use_angle_cls=False,
+        lang='japan',
+        show_log=False
+    )
+
+ocr = load_ocr()
+
+uploaded_file = st.file_uploader(
+    "画像アップロード",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file:
+
+    image = Image.open(uploaded_file).convert("RGB")
+
+    st.image(image)
+
+    img_array = np.array(image)
+
+    with st.spinner("OCR中..."):
+        result = ocr.ocr(img_array)
+
+    texts = []
+
+    if result and result[0]:
+        for line in result[0]:
+            texts.append(line[1][0])
+
+    st.text_area(
+        "結果",
+        "\n".join(texts),
+        height=300
+    )
